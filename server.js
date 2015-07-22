90d60ae349ee4eb1e57239164bcb171e6c28c1bf
andrewxie <13761224431@163.com>
2015-07-22 15:29:27 +0000

    [API] Socket.io up and running.

--- server.js
+++ server.js
@@ -6,10 +6,23 @@ var app = express();
 var server = http.createServer(app);
 server.listen(process.env.PORT, process.env.IP);//To work on c9.io
 
+var io = require("socket.io")(server);
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
-process.on('SIGINT',function(){
-    console.log("Existed from web server.");
+io.on("connection", function(socket){
+    console.log("One user connected.");
+    
+    socket.on("disconnect", function() {
+        console.log("One user disconnected.");
+    });
 });
+
+process.on('SIGINT',function(){
+    console.log("\nExisted from web server.");
+    process.exit();
+});
+
+
